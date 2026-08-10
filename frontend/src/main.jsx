eb08a6747ab9219f20f2729262a3fb2a77f3f773
Jhon Carlo Canulo <canulo1234@gmail.com>
2026-08-10 14:26:13 +0800

    install pageinsight

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
 import "./index.css";
 import App from "./App.jsx";
-import { injectSpeedInsights } from "@vercel/speed-insights";
 
 injectSpeedInsights();
 
