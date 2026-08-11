3a07f9f33b532bc213d30b111fc74c6314ad315b
Jhon Carlo Canulo <canulo1234@gmail.com>
2026-08-11 11:34:22 +0800

    update certification migration

--- backend/database/migrations/2026_08_10_074314_create_certification_table.php
+++ backend/database/migrations/2026_08_10_074314_create_certification_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('certification', function (Blueprint $table) {
+        Schema::create('certifications', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('provider');
