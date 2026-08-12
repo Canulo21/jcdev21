059dfc8ada16950ef6fde8cf3707d79c393d21b2
Jhon Carlo Canulo <canulo1234@gmail.com>
2026-08-12 20:42:02 +0800

    fix

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            CategorySeeder::class,
             ProjectSeeder::class,
             TagSeeder::class,
             ProjectTagSeeder::class,
-            CategorySeeder::class,
             CertificationSeeder::class,
             CompanySeeder::class,    
             CompanyExperienceSeeder::class,
