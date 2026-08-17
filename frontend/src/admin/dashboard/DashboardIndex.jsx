973c5151d999b0bb6bea1e6b5815b841cd0756e6
Jhon Carlo Canulo <canulo1234@gmail.com>
2026-08-17 19:50:26 +0800

    improved

--- frontend/src/admin/dashboard/DashboardIndex.jsx
+++ frontend/src/admin/dashboard/DashboardIndex.jsx
@@ -1,8 +1,46 @@
 import { Separator } from "@/components/ui/separator";
-import React from "react";
+import apiFetch from "@/lib/api";
+import React, { useEffect, useState } from "react";
 import { FaCode, FaNewspaper, FaWordpress } from "react-icons/fa";
 
 function DashboardIndex() {
+  const [totalWPProj, setTotalWPProj] = useState(0);
+  const [totalFullStack, setTotalFullStack] = useState(0);
+  const [totalFrontend, setTotalFrontend] = useState(0);
+  const [totalCertificates, setTotalCertificates] = useState(0);
+
+  const getProjects = async () => {
+    try {
+      const data = await apiFetch("projects");
+
+      const filterWp = data.filter((wp) => wp.category.name === "WordPress");
+      const filterFullStack = data.filter(
+        (fstack) => fstack.category.name === "Full Stack",
+      );
+      const filterFend = data.filter((fn) => fn.category.name === "Frontend");
+
+      setTotalWPProj(filterWp.length);
+      setTotalFullStack(filterFullStack.length);
+      setTotalFrontend(filterFend.length);
+    } catch (err) {
+      console.error("Failed to fetch certificates:", err);
+    }
+  };
+
+  const getCertificates = async () => {
+    try {
+      const data = await apiFetch("certifications");
+      setTotalCertificates(data.length);
+    } catch (err) {
+      console.error("Failed to fetch certificates:", err);
+    }
+  };
+
+  useEffect(() => {
+    getProjects();
+    getCertificates();
+  }, []);
+
   return (
     <>
       <h1 className="!mb-15">Welcom Master JC!</h1>
@@ -15,7 +53,7 @@ function DashboardIndex() {
             <p className="font-medium">WordPress</p>
           </div>
           <div className="p-6 ">
-            <p className="text-4xl font-medium">15</p>
+            <p className="text-4xl font-medium">{totalWPProj}</p>
           </div>
         </div>
         <div className="flex items-normal">
@@ -24,7 +62,7 @@ function DashboardIndex() {
             <p className="font-medium">Frontend</p>
           </div>
           <div className="p-6 ">
-            <p className="text-4xl font-medium">15</p>
+            <p className="text-4xl font-medium">{totalFrontend}</p>
           </div>
         </div>
         <div className="flex items-normal">
@@ -33,7 +71,7 @@ function DashboardIndex() {
             <p className="font-medium">Fullstack</p>
           </div>
           <div className="p-6 ">
-            <p className="text-4xl font-medium">15</p>
+            <p className="text-4xl font-medium">{totalFullStack}</p>
           </div>
         </div>
       </div>
@@ -48,7 +86,7 @@ function DashboardIndex() {
             <p className="font-medium">Certificates</p>
           </div>
           <div className="p-6 ">
-            <p className="text-4xl font-medium">15</p>
+            <p className="text-4xl font-medium">{totalCertificates}</p>
           </div>
         </div>
       </div>
